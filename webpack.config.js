b1c822eac901925f8c08d20b4063297bb105e09d
Dmitry Kravcev <tpdigmma@gmail.com>
2024-06-14 13:00:26 +0300

    release v0.2.3 - В сборку подключен babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
-
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    context:  path.resolve(__dirname, 'src'),
+    context: path.resolve(__dirname, 'src'),
     entry: './index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Обрабатываем все JS файлы
+                exclude: /node_modules/, // Исключаем папку node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
     devServer: {
         hot: true,
         static: {
-            directory: './dist',
+            directory: path.resolve(__dirname, 'dist'),
             watch: true
         }
     }
-}
+};
